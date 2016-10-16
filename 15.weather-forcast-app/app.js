cb33eef456ceaeafddf6790e94be75d68d01f56f
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2016-10-16 22:49:24 +0600

    Working weather forecast app

--- 15.weather-forcast-app/app.js
+++ 15.weather-forcast-app/app.js
@@ -15,7 +15,7 @@ weatherApp.config(function ($routeProvider) {
 });
 
 weatherApp.service('cityService', function () {
-	this.cityname = 'New Yourk, NY';
+	this.cityname = 'Dhaka';
 });
 
 weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
@@ -26,6 +26,12 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function ($sco
 	});
 }]);
 
-weatherApp.controller('forcastController', ['$scope', 'cityService', function ($scope, cityService) {
+weatherApp.controller('forcastController', ['$scope', '$resource', 'cityService', function ($scope, $resource, cityService) {
 	$scope.cityname = cityService.cityname;
+
+	// $scope.weatherAPI = $resource('http://api.worldweatheronline.com/free/v1/weather.ashx?q=Karachi&format=JSON&extra=undefined&num_of_days=2&date=&fx=&cc=&includelocation=&show_comments=&key=xkq544hkar4m69qujdgujn7w', { callback: 'JSON_CALLBACK' }, { get: { method: 'JSONP' }});
+
+	$scope.weatherAPI = $resource('http://api.worldweatheronline.com/free/v1/weather.ashx?q='+$scope.cityname+'&key=xkq544hkar4m69qujdgujn7w', { callback: 'JSON_CALLBACK' }, { get: { method: 'JSONP' }});
+
+	$scope.weatherResults = $scope.weatherAPI.get({ format: 'JSON', num_of_days: 2 });
 }]);
